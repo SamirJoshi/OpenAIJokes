ca72ce00092363fe7e68fe87de294d7a2b14b7cb
brandonfujii <brandonfujii2018@u.northwestern.edu>
2019-03-07 15:20:08 -0600

    [Minor] Added function to load from local .npy file, removed num_jokes attribute, added default overrided arguments for create_npy, display_info

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ import argparse
 class Dataset:
     def __init__(self, identifier=''):
         self.jokes = []
-        self.num_jokes = 0
         self.identifier = identifier
 
     def __str__(self):
@@ -17,28 +16,20 @@ class Dataset:
 
         return dataset_info
 
+    @property
+    def num_jokes(self):
+        return len(self.jokes)
+
     def load_from_json(self, path):
         with open(path, 'r', encoding='utf-8') as json_file:
-            self.jokes = json.load(json_file)
-
-        self.num_jokes = len(self.jokes)
+            self.jokes = np.asarray(json.load(json_file))
 
     def load_from_npy_file(self, path):
-        raise NotImplementedError
+        self.jokes = np.load(path)
 
     def write_to_npy_file(self, path=''):
         filename = path + self.identifier + '_dataset.npy'
         np.save(filename, self.jokes)
-
-    def load_dataset(self, filename, isNPyCache=False):
-        """
-        If the npy file is found, load from there. Else, load from json.
-        If json is not found, throw
-        """
-        if isNPyCache:
-            self.load_from_npy_file(filename)
-        else:
-            self.load_from_json(filename)
 
 
 def generate_parser():
@@ -48,30 +39,29 @@ def generate_parser():
             type=str,
             default='jokes_dataset',
             help='an identifier for the dataset')
-    parser.add_argument('--is_npy',
-            type=bool,
-            default=False,
-            help='is the file of jokes a numpy cache')
     parser.add_argument('--create_npy',
-            type=bool,
             default=False,
+            action='store_true',
             help='is the file of jokes a numpy cache')
     parser.add_argument('--display_info',
-            type=bool,
             default=False,
-            help='print te stats of the dataset after loading')
+            action='store_true',
+            help='print the stats of the dataset after loading')
 
     return parser
 
 if __name__ == '__main__':
     parser = generate_parser()
     args = parser.parse_args()
-
     dataset = Dataset(args.identifier)
-    dataset.load_dataset(args.jokes_path, args.is_npy)
+    file_ext = args.jokes_path.rpartition('.')[-1]
 
-    if (not args.is_npy) and args.create_npy:
-        dataset.write_to_npy_file()
+    if file_ext == 'npy':
+        dataset.load_from_npy_file(args.jokes_path)
+    else:
+        dataset.load_from_json(args.jokes_path)
+        if args.create_npy:
+            dataset.write_to_npy_file()
 
     if args.display_info:
         print(dataset)
